6a0884fcb9e03bf4a8b32093dc312f10e180eeb4
izaakdale <izaakdale@live.com>
2022-03-30 01:53:54 -0700

    Add order webhook handling

--- app/Handlers/WebhookHandler.php
+++ app/Handlers/WebhookHandler.php
@@ -2,15 +2,28 @@
 
 namespace App\Handlers;
 
-use App\Models\User;
+use App\Models\Order;
+use App\Models\OrderItem;
 use Spatie\WebhookClient\Jobs\ProcessWebhookJob;
 
 class WebhookHandler extends ProcessWebhookJob {
 
     public function handle(){
         
-        
+        $order_data = $this->webhookCall['payload']['order_data'];
+        $incomingOrder = new Order();
 
+        $incomingOrder->user_id = $order_data['user_id'];
+        $incomingOrder->save();
+
+        foreach( $order_data['items'] as $product )
+        {
+            $incomingOrderItem = new OrderItem();
+            $incomingOrderItem->order_id = $incomingOrder->id;
+            $incomingOrderItem->product_id = $product['product_id'];
+            $incomingOrderItem->quantity = $product['quantity'];
+            $incomingOrderItem->save();
+        }
     }
 }
 
